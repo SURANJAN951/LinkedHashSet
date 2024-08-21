972c98978e1e0ac604052d2f799319cf846f2656
Suranjan kumar <ksuranjan951@gmail.com>
2024-08-21 12:10:32 +0530

    LinkedHashSet

---
+++ LinkedHashSetEx.java
@@ -0,0 +1,12 @@
+import java.util.LinkedHashSet;
+
+public class LinkedHashSetEx {
+    public static void main(String[] args) {
+        LinkedHashSet hs1=new LinkedHashSet();
+        hs1.add(100);
+        hs1.add(20);
+        hs1.add(30);
+        hs1.add(40);
+        System.out.println(hs1);
+    }
+}
